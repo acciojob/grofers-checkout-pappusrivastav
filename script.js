e88665df489b277812b98e9416d29f71cb4a8eac
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-03 18:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
+
+// const getSum = () => {
+// //Add your code here
+  
+// };
+
+// getSumBtn.addEventListener("click", getSum);
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    const prices = document.querySelectorAll(".price");
+    let total = 0;
+    prices.forEach((price) => {
+        total += parseInt(price.textContent);
+    });
+
+    const table = document.querySelector("table");
+    const newRow = table.insertRow(-1);
+    const cell = newRow.insertCell(0);
+    cell.textContent = `Total Price: ${total}`;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
